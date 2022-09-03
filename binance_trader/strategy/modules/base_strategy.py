0058ddfa5876041147f524532c762f3bdb4890e2
Rishabh Mittal <mittalrishabh181@gmail.com>
2022-09-04 05:07:47 +0530

    Ideating on how to go about the architecture

--- binance_trader/strategy/modules/base_strategy.py
+++ binance_trader/strategy/modules/base_strategy.py
@@ -1,6 +1,15 @@
 class BaseStrategy:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, interval, symbol) -> None:
+        self._interval = interval
+        self._symbol = symbol
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @property
+    def interval(self):
+        return self._interval
 
     def buy_signal(self):
         pass
