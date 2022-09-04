113211293a73fdfd01d396c5ff8970246d478f3d
Rishabh Mittal <mittalrishabh181@gmail.com>
2022-09-04 17:32:49 +0530

    setup base strategy for inheritance in custom strategies

--- binance_trader/strategy/modules/base_strategy.py
+++ binance_trader/strategy/modules/base_strategy.py
@@ -1,7 +1,8 @@
 import asyncio
+import csv
+import datetime as dt
 import decimal
-import json
-from pprint import pprint
+import os
 
 import pandas as pd
 from binance import AsyncClient
@@ -21,15 +22,25 @@ from keys import Keys
 class BaseStrategy:
     @classmethod
     async def create(
-        cls, api_key: str, api_secret: str, testnet: bool, interval: str, symbol: str
+        cls,
+        api_key: str,
+        api_secret: str,
+        testnet: bool,
+        interval: str,
+        symbol: str,
+        order_log_loc: str,
     ):
         self = BaseStrategy()
         self._interval = interval
         self._symbol = symbol
+        self._api_key = api_key
+        self._api_secret = (api_secret,)
+        self._testnet = testnet
         self.async_client = await AsyncClient().create(
             api_key=api_key, api_secret=api_secret, testnet=testnet
         )
-        self.symbol_info, self.filters = await self.symbol_info()
+        self._symbol_info, self._filters = await self.symbol_info()
+        self._order_log_location = order_log_loc
         return self
 
     @property
@@ -40,6 +51,18 @@ class BaseStrategy:
     def interval(self):
         return self._interval
 
+    @property
+    def symbol_info(self):
+        return self._symbol_info
+
+    @property
+    def filters(self):
+        return self._filters
+
+    @property
+    def order_log_location(self):
+        return self._order_log_location
+
     async def symbol_info(self):
         symbol_info = await self.async_client.get_symbol_info(symbol=self.symbol)
         filters = pd.DataFrame(symbol_info["filters"]).set_index(
@@ -47,32 +70,56 @@ class BaseStrategy:
         )
         return (symbol_info, filters)
 
-    async def min_qty(self):
+    async def _min_qty(self):
         min_notional_val = float(self.filters.loc["MIN_NOTIONAL"]["minNotional"])
         avg_price_dict = await self.async_client.get_avg_price(symbol=self.symbol)
         avg_price = float(avg_price_dict["price"])
-        return min_notional_val / avg_price
+        return (min_notional_val / avg_price, avg_price)
 
-    async def process_qty(self, qty):
-        min_qty = await self.min_qty()
+    async def _process_qty(self, qty):
+        min_qty, avg_price = await self._min_qty()
         round_to_digits = float(self.filters.loc["PRICE_FILTER"]["tickSize"])
         final_qty = round_step_size(qty, round_to_digits)
+
         if final_qty < min_qty:
             raise ValueError(f"{final_qty} is less than than {min_qty=}")
         return final_qty
 
     async def create_new_order(self, side: Side, type: FutureOrder, quantity: decimal):
-        qty = await self.process_qty(quantity)
+        qty = await self._process_qty(quantity)
+
         try:
             order_result = await self.async_client.futures_create_order(
                 symbol=self.symbol, side=side, type=type, quantity=qty
             )
+            self._file_writer(order_result, "order_log.csv")
+
         except BinanceAPIException as e:
             print(e)
-        else:
-            print(json.dumps(order_result))
+            await self.async_client.close_connection()
+            return None
 
         return qty
+
+    def _file_writer(self, row, file_name):
+        try:
+            file_exists = os.path.isfile(f"{self.order_log_location}/{file_name}")
+            with open(f"{self.order_log_location}/{file_name}", "a") as file:
+                writer = csv.DictWriter(
+                    file,
+                    delimiter=",",
+                    lineterminator="\n",
+                    fieldnames=list(row.keys()),
+                )
+
+                if not file_exists:
+                    print(f"Creating file @ {self.order_log_location}/{file_name}....")
+                    writer.writeheader()
+
+                print(f"{dt.datetime.now()} Writing to File: {file_name}")
+                writer.writerow(row)
+        except Exception as e:
+            print(e)
 
 
 async def BaseStrategyRun(
@@ -84,9 +131,15 @@ async def BaseStrategyRun(
         testnet=testnet,
         interval=interval,
         symbol=symbol,
+        order_log_loc="/home/rishabh/projects/binance-trader/binance_trader/data/db/account",
     )
 
-    await base_strat_obj.create_new_order(Side.Sell, FutureOrder.Market, 0.01)
+    await base_strat_obj.create_new_order(Side.Buy, FutureOrder.Market, 0.05)
+    await asyncio.sleep(5)
+    await base_strat_obj.create_new_order(Side.Sell, FutureOrder.Market, 0.05)
+    await asyncio.sleep(5)
+    await base_strat_obj.create_new_order(Side.Buy, FutureOrder.Market, 0.05)
+
     await base_strat_obj.async_client.close_connection()
 
 
@@ -100,7 +153,6 @@ if __name__ == "__main__":
             symbol="BTCUSDT",
         )
     )
-
     # asyncio.run(base_strat.close_connection())
     # asyncio.run(base_strat.start_connection())
     # asyncio.run(base_strat.async_client.futures_account())
--- binance_trader/strategy/modules/custom_strategy/sma_crossover.py
+++ binance_trader/strategy/modules/custom_strategy/sma_crossover.py
@@ -1,6 +1,75 @@
+import asyncio
+import datetime as dt
+
 import pandas as pd
+from binance import AsyncClient
+from binance.enums import *
+from binance.exceptions import BinanceAPIException
+from binance.helpers import round_step_size
+from binance_trader.strategy.modules.base_strategy import BaseStrategy
+from binance_trader.strategy.modules.models.models import (
+    FutureOrder,
+    OrderFillType,
+    OrderResponse,
+    OrderStatus,
+    Side,
+)
+from keys import Keys
 
 
-class SMACrossover:
-    def __init__(self) -> None:
-        pass
+class SMACrossover(BaseStrategy):
+    @classmethod
+    async def create(
+        cls,
+        api_key: str,
+        api_secret: str,
+        testnet: bool,
+        interval: str,
+        symbol: str,
+        order_log_loc: str,
+    ):
+        self = BaseStrategy()
+        self._interval = interval
+        self._symbol = symbol
+        self._api_key = api_key
+        self._api_secret = (api_secret,)
+        self._testnet = testnet
+        self.async_client = await AsyncClient().create(
+            api_key=api_key, api_secret=api_secret, testnet=testnet
+        )
+        self._symbol_info, self._filters = await self.symbol_info()
+        self._order_log_location = order_log_loc
+        return self
+
+
+async def SMAStrategyRun(
+    api_key: str, api_secret: str, testnet: bool, interval: str, symbol: str
+):
+    base_strat_obj = await SMACrossover.create(
+        api_key=api_key,
+        api_secret=api_secret,
+        testnet=testnet,
+        interval=interval,
+        symbol=symbol,
+        order_log_loc="/home/rishabh/projects/binance-trader/binance_trader/data/db/account",
+    )
+
+    await base_strat_obj.create_new_order(Side.Buy, FutureOrder.Market, 0.05)
+    await asyncio.sleep(5)
+    await base_strat_obj.create_new_order(Side.Sell, FutureOrder.Market, 0.05)
+    await asyncio.sleep(5)
+    await base_strat_obj.create_new_order(Side.Buy, FutureOrder.Market, 0.05)
+
+    await base_strat_obj.async_client.close_connection()
+
+
+if __name__ == "__main__":
+    asyncio.run(
+        SMAStrategyRun(
+            api_key=Keys.API,
+            api_secret=Keys.SECRET,
+            testnet=True,
+            interval=AsyncClient.KLINE_INTERVAL_1MINUTE,
+            symbol="BTCUSDT",
+        )
+    )
